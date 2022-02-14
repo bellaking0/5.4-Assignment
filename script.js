a7c66a8b1948fa6b301dd02b8db6234c2893507b
bellaking0 <77692024+bellaking0@users.noreply.github.com>
2022-02-14 15:05:07 -0600

    commit to 'main'

---
+++ script.js
@@ -0,0 +1,10 @@
+const names = ["Gary", "Spongebob", "Patrick", "Squidward"];
+let x = 0;
+let text = "x";
+for (let x = 0; x < 4; x++) {
+    Text += "Your Spongebob Squarepants character is" + x + "<p>"
+}
+while (names[x]) {
+    text += names[x];
+    x++;
+}
